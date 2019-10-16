8a0a0869da50c57a94e52f20ab026893746fc121
Sérgio Salgado <sergiodias.17@hotmail.com>
2019-10-16 16:58:31 +0100

    Add test QRCode generator.

--- AcmeSupermarket/CustomerApp/app/src/main/java/org/feup/cmov/acmecustomer/MainActivity.java
+++ AcmeSupermarket/CustomerApp/app/src/main/java/org/feup/cmov/acmecustomer/MainActivity.java
@@ -2,7 +2,16 @@ package org.feup.cmov.acmecustomer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.generateQRCode();
     }
+
+    private void generateQRCode() {
+        QRCodeWriter writer = new QRCodeWriter();
+        try {
+            BitMatrix bitMatrix = writer.encode("teste", BarcodeFormat.QR_CODE, 512, 512);
+            int width = bitMatrix.getWidth();
+            int height = bitMatrix.getHeight();
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                }
+            }
+            ((ImageView) findViewById(R.id.img_result_qr)).setImageBitmap(bmp);
+
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
