329b1c9ebe7042d340a175f7aaef8d46d149b867
Sérgio Salgado <sergiodias.17@hotmail.com>
2019-10-21 17:27:49 +0100

    Add Registration Layout.

--- AcmeSupermarket/CustomerApp/app/src/main/java/org/feup/cmov/acmecustomer/activities/MainActivity.java
+++ AcmeSupermarket/CustomerApp/app/src/main/java/org/feup/cmov/acmecustomer/activities/MainActivity.java
@@ -2,15 +2,7 @@ package org.feup.cmov.acmecustomer.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.ImageView;
-
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
 
 import org.feup.cmov.acmecustomer.R;
 
@@ -20,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
         //this.generateQRCode();
     }
 
