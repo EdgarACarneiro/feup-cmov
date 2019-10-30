499e7ca27efbdafca3f464b7cc9e6dcc0ee044f3
Sérgio Salgado <sergiodias.17@hotmail.com>
2019-10-30 02:05:54 +0000

    Login screen checking input and switching around login and register screen.

--- AcmeSupermarket/CustomerApp/app/src/main/java/org/feup/cmov/acmecustomer/activities/LoginActivity.java
+++ AcmeSupermarket/CustomerApp/app/src/main/java/org/feup/cmov/acmecustomer/activities/LoginActivity.java
@@ -2,7 +2,12 @@ package org.feup.cmov.acmecustomer.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import org.feup.cmov.acmecustomer.R;
 
@@ -12,5 +17,56 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView login = findViewById(R.id.dont_have_account);
+        login.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        changeToRegisterScreen();
+                    }
+                }
+        );
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        onLogin();
+                    }
+                }
+        );
+    }
+
+    protected void changeToRegisterScreen() {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    protected void onLogin() {
+        if(noErrorsOnLogin()) {
+            TextView errorMessage = findViewById(R.id.error_message);
+            errorMessage.setText("");
+            errorMessage.setVisibility(View.GONE);
+
+            //missing REST call to login
+
+            /*Intent intent = new Intent(this, MainScreen.class);
+            startActivity(intent);*/
+        } else {
+            TextView errorMessage = findViewById(R.id.error_message);
+            errorMessage.setText("Please verify the data before submit!");
+            errorMessage.setVisibility(View.VISIBLE);
+        }
+    }
+
+    protected boolean noErrorsOnLogin() {
+        String username = ((EditText)findViewById(R.id.input_username)).getText().toString();
+        String password = ((EditText)findViewById(R.id.input_password)).getText().toString();
+
+        //missing server response
+
+        return username.length() > 3 && password.length() >= 5;
     }
 }
