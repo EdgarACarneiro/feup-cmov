2d92ec42329cbb462123232bd50d0fb9425e92fd
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2019-11-05 23:12:26 +0000

    Signing list of products

--- AcmeSupermarket/CustomerApp/app/src/main/java/org/feup/cmov/acmecustomer/models/ShoppingCart.java
+++ AcmeSupermarket/CustomerApp/app/src/main/java/org/feup/cmov/acmecustomer/models/ShoppingCart.java
@@ -1,19 +1,14 @@
 package org.feup.cmov.acmecustomer.models;
 
-import android.security.keystore.KeyProperties;
+
 
 import org.feup.cmov.acmecustomer.interfaces.QRCodeInterface;
 
-import java.io.ByteArrayOutputStream;
 import java.io.Serializable;
 import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
-import java.security.Key;
 import java.security.PrivateKey;
+import java.security.Signature;
 import java.util.ArrayList;
-import java.util.Base64;
-
-import javax.crypto.Cipher;
 
 public class ShoppingCart implements Serializable, QRCodeInterface {
     private ArrayList<Product> products;
@@ -42,38 +37,42 @@ public class ShoppingCart implements Serializable, QRCodeInterface {
         return value;
     }
 
+    private byte[] concaByteArrays(byte[] a, byte[] b) {
+        byte[] res = new byte[a.length + b.length];
+        System.arraycopy(a, 0, res, 0, a.length);
+        System.arraycopy(b, 0, res, a.length, b.length);
+
+        return res;
+    }
+
     public byte[] encode(PrivateKey privateKey) {
         final int ACME_TAG_LENGTH = 4;
-        final int UUID_LENGTH = 16;
+        final int PRODUCT_CODE = 16;
         final int PRICE_EURO_LENGTH = 4;
         final int PRICE_CENTS_LENGTH = 4;
-        final int DESCRIPTION_SIZE_LENGTH = 1;
-        final String ENC_ALGO = "RSA/NONE/PKCS1Padding";
         ByteBuffer tag;
-        int tagLength = 0;
+        int tagsLength = 0;
 
-        for(int i = 0; /*i < this.products.size()*/ i < 1; i++) {
-            tagLength += ACME_TAG_LENGTH + UUID_LENGTH + PRICE_EURO_LENGTH + PRICE_CENTS_LENGTH + DESCRIPTION_SIZE_LENGTH + this.products.get(i).getName().length();
+        for(int i = 0; i < this.products.size(); i++) {
+            tagsLength += ACME_TAG_LENGTH + PRODUCT_CODE + PRICE_EURO_LENGTH + PRICE_CENTS_LENGTH;
         }
 
-        tag = ByteBuffer.allocate(tagLength);
+        tag = ByteBuffer.allocate(tagsLength);
 
-        for(int i = 0; /*i < this.products.size()*/ i < 1; i++) {
+        for(int i = 0; i < this.products.size(); i++) {
             tag.putInt(0x41636D65);
             tag.putLong(this.products.get(i).getUUID().getMostSignificantBits());
             tag.putLong(this.products.get(i).getUUID().getLeastSignificantBits());
             tag.putInt(this.products.get(i).getPrice().getEuros());
             tag.putInt(this.products.get(i).getPrice().getCents());
-            tag.put((byte)this.products.get(i).getName().length());
-            tag.put(this.products.get(i).getName().getBytes(StandardCharsets.ISO_8859_1));
         }
 
         try {
-            Cipher cipher = Cipher.getInstance(ENC_ALGO);
-            cipher.init(Cipher.ENCRYPT_MODE, privateKey);
-            byte[] encTag = cipher.doFinal(tag.array());
-            System.out.println("ENCRYPTED TAG: " + StandardCharsets.ISO_8859_1.decode(tag).toString());
-            return encTag;
+            Signature sig = Signature.getInstance("SHA256WithRSA");
+            sig.initSign(privateKey);
+            sig.update(tag);
+            return concaByteArrays(tag.array(), sig.sign());
+
         } catch (Exception e) {
             e.printStackTrace();
         }
