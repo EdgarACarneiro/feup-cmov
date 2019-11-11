c3446cb0e09c3ea7343ec9f38cbd1eb133196f2b
Sérgio Salgado <sergiodias.17@hotmail.com>
2019-11-11 04:17:01 +0000

    Add Undo option after removing product from shopping cart

--- AcmeSupermarket/CustomerApp/app/src/main/java/org/feup/cmov/acmecustomer/adapters/ShoppingListAdapter.java
+++ AcmeSupermarket/CustomerApp/app/src/main/java/org/feup/cmov/acmecustomer/adapters/ShoppingListAdapter.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.feup.cmov.acmecustomer.R;
 import org.feup.cmov.acmecustomer.activities.MainMenuActivity;
@@ -20,6 +21,7 @@ import java.util.Locale;
 public class ShoppingListAdapter extends RecyclerView.Adapter<ShoppingListAdapter.ShoppingListViewHolder> {
     private Customer customer;
     private Product recentlyDeletedProduct;
+    private int recentlyDeletedProductIndex;
     private Context context;
 
     public static class ShoppingListViewHolder extends RecyclerView.ViewHolder {
@@ -57,8 +59,27 @@ public class ShoppingListAdapter extends RecyclerView.Adapter<ShoppingListAdapte
 
     public void deleteProduct(int position) {
         this.recentlyDeletedProduct = this.customer.getShoppingCart().getProducts().get(position);
+        this.recentlyDeletedProductIndex = position;
         this.customer.getShoppingCart().removeProduct(position);
         notifyItemRemoved(position);
+        if(context instanceof MainMenuActivity) {
+            updateButtons();
+            ((MainMenuActivity) context).updateCartValue();
+        }
+        showUndoSnackbar();
+    }
+
+    public void showUndoSnackbar() {
+        View view = ((MainMenuActivity) context).findViewById(R.id.coordinator_layout);
+        CharSequence cs = this.recentlyDeletedProduct.getName() + " removed from the shopping cart";
+        Snackbar snackbar = Snackbar.make(view, cs, Snackbar.LENGTH_LONG);
+        snackbar.setAction("Undo", v -> undoDeleteProduct());
+        snackbar.show();
+    }
+
+    public void undoDeleteProduct() {
+        this.customer.getShoppingCart().getProducts().add(this.recentlyDeletedProductIndex, this.recentlyDeletedProduct);
+        notifyItemInserted(this.recentlyDeletedProductIndex);
         if(context instanceof MainMenuActivity) {
             updateButtons();
             ((MainMenuActivity) context).updateCartValue();
