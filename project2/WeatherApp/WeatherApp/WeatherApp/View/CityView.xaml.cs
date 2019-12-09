f47c170d972ac71151c50db412ea7d3f9aa97c88
Sérgio Salgado <sergiodias.17@hotmail.com>
2019-12-09 22:38:45 +0000

    Drawing line graph

--- project2/WeatherApp/WeatherApp/WeatherApp/View/CityView.xaml.cs
+++ project2/WeatherApp/WeatherApp/WeatherApp/View/CityView.xaml.cs
@@ -53,6 +53,33 @@ namespace WeatherApp.View
                 TextSize = 30
             };
 
+            SKPaint coorPaint2 = new SKPaint
+            {      // paint for the axis and text
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Gray,
+                StrokeWidth = 2,
+                StrokeCap = SKStrokeCap.Round,
+                TextSize = 30
+            };
+
+            SKPaint graphPaint = new SKPaint
+            {      // paint for the axis and text
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Blue,
+                StrokeWidth = 5,
+                StrokeCap = SKStrokeCap.Round,
+                TextSize = 30
+            };
+
+            SKPaint graphPaint2 = new SKPaint
+            {      // paint for the axis and text
+                Style = SKPaintStyle.StrokeAndFill,
+                Color = SKColors.Blue,
+                StrokeWidth = 5,
+                StrokeCap = SKStrokeCap.Round,
+                TextSize = 30
+            };
+
             SKPoint origin = new SKPoint(topLeft.X, bottomRight.Y);
 
             //draw X and Y axis;
@@ -65,115 +92,43 @@ namespace WeatherApp.View
             for(int i = 0; i < steps; i++)
             {
                 canvas.DrawLine(new SKPoint(origin.X + i * stepX, origin.Y), new SKPoint(origin.X + i * stepX, origin.Y + 15), coorPaint); //draw guide lines on X axis
-                canvas.DrawText(hours[i], origin.X + i * stepX + 5, origin.Y + 35, coorPaint); //draw '00h' text
+                canvas.DrawText(hours[i], origin.X + i * stepX + stepX / 3, origin.Y + 40, coorPaint); //draw '00h' text
             }
 
-            //draw guide lines on Y axis
             float maxTemp = temps.Max();
             float minTemp = temps.Min();
+            //calculate temperature value per pixel in Y axis
+            float pixelPerDegree = (topLeft.Y - origin.Y) / (maxTemp - minTemp);
+            float stepY = (origin.Y - topLeft.Y) / 4;
+
+            //draw guide lines on Y axis
+            for(int i = 0; i < 4; i++)
+            {
+                float guidelineTemp = maxTemp - i * ((maxTemp - minTemp) / 4);
+                canvas.DrawLine(new SKPoint(origin.X, topLeft.Y + 30 + i * stepY), new SKPoint(bottomRight.X, topLeft.Y + 30 + i * stepY), coorPaint2);
+                if(guidelineTemp != maxTemp && guidelineTemp != minTemp)
+                {
+                    canvas.DrawText(guidelineTemp.ToString(), origin.X - 80, topLeft.Y + 30 + i * stepY, coorPaint);
+                }
+            }
 
             canvas.DrawLine(origin, new SKPoint(origin.X - 15, origin.Y), coorPaint);
             canvas.DrawLine(new SKPoint(topLeft.X, topLeft.Y + 30), new SKPoint(topLeft.X - 15, topLeft.Y + 30), coorPaint);
-            canvas.DrawText(minTemp.ToString(), origin.X - 60, origin.Y - 15, coorPaint); //estes + e - 15 são ajustes para tornar o número mais legível
-            canvas.DrawText(maxTemp.ToString(), topLeft.X - 60, topLeft.Y + 15, coorPaint);
+            canvas.DrawText(minTemp.ToString(), origin.X - 80, origin.Y - 15, coorPaint); //estes + e - 15 são ajustes para tornar o número mais legível
+            canvas.DrawText(maxTemp.ToString(), topLeft.X - 80, topLeft.Y + 15, coorPaint);
 
-            //calculate temperature value per pixel in Y axis
-            float pixelPerDegree = (topLeft.Y - origin.Y) / (maxTemp - minTemp);
-
-            for(int i = 0; i < temps.Count(); i++)
-            {
-                if(temps[i] != minTemp && temps[i] != maxTemp)
-                {
-                    canvas.DrawText(temps[i].ToString(), origin.X - 60, origin.Y + (temps[i] - minTemp) * pixelPerDegree - 15, coorPaint);
-                }
-            }
-        }
-
-        /*void DrawAxis(SKCanvas cnv, int wd, int hg, float pxP, out float one, out float xhg)
-        {
-            
-            int ylen;    // total size of the Y axis (in pixels) with a maximum of 500px for 1 and another 500px for -1 + 20px after 1 and -1
-
-            SKPaint coorPaint = new SKPaint
-            {      // paint for the axis and text
-                Style = SKPaintStyle.Stroke,
-                Color = SKColors.Black,
-                StrokeWidth = 3,
-                StrokeCap = SKStrokeCap.Square,
-                TextSize = 2 * margin
-            };
-
-            ylen = Math.Min(2 * graphH + 4 * margin, hg - 2 * margin);
-            one = (float)ylen - 4 * margin;
-            xhg = (float)hg - (hg - ylen) / 2 - 2 * margin;
-
-            cnv.DrawLine(margin, xhg - one, 3 * margin, xhg - one, coorPaint);  // 1 mark on the Y axis
-            cnv.DrawText("1", 3.5f * margin, xhg - one, coorPaint);             // 1 label on the Y axis
-            for (int k = 1; k <= ncycles; k++)
-            {                                // marks and labels on the X axis
-                cnv.DrawLine(2 * margin + k * pxP, xhg - margin / 2, 2 * margin + k * pxP, xhg + margin / 2, coorPaint);
-                cnv.DrawText(k.ToString(), 1.5f * margin + k * pxP, xhg + 3 * margin, coorPaint);
-            }
-            cnv.DrawText("periods", 3 * margin, xhg + 2.5f * margin, coorPaint);     // 'periods' indication on the X axis
-        }
-
-        void DrawGraph(SKCanvas cnv, int wd, int hg, float pxP, float one, float yhg)
-        {
-            SKPaint gPaint = new SKPaint
-            {        // paint for the graphic
-                Style = SKPaintStyle.Stroke,
-                Color = SKColors.Red,
-                StrokeWidth = 1,
-                StrokeCap = SKStrokeCap.Butt
-            };
-
-            float period = (float)(2 * Math.PI);     // period (math in radians) for the Sinus
-            SKPath path = new SKPath();
-            path.MoveTo(2 * margin, yhg);            // point (0, 0) on the graphic
-
-            cnv.DrawPath(path, gPaint);
-        }
-
-        void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
-        {
-            SKImageInfo info = args.Info;
-            SKSurface surface = args.Surface;
-            SKCanvas canvas = surface.Canvas;
-
-            canvas.Clear();
-
-            // Create the path
             SKPath path = new SKPath();
 
-            // Define the first contour
-            path.MoveTo(0.5f * info.Width, 0.1f * info.Height);
-            path.LineTo(0.2f * info.Width, 0.4f * info.Height);
-            path.LineTo(0.8f * info.Width, 0.4f * info.Height);
-            path.LineTo(0.5f * info.Width, 0.1f * info.Height);
+            path.MoveTo(origin.X + stepX / 2, origin.Y + (temps[0] - minTemp) * pixelPerDegree);
+            canvas.DrawCircle(new SKPoint(origin.X + stepX / 2, origin.Y + (temps[0] - minTemp) * pixelPerDegree), 10, graphPaint2);
 
-            // Define the second contour
-            path.MoveTo(0.5f * info.Width, 0.6f * info.Height);
-            path.LineTo(0.2f * info.Width, 0.9f * info.Height);
-            path.LineTo(0.8f * info.Width, 0.9f * info.Height);
-            path.Close();
-
-            // Create two SKPaint objects
-            SKPaint strokePaint = new SKPaint
+            for(int i = 1; i < temps.Count(); i++)
             {
-                Style = SKPaintStyle.Stroke,
-                Color = SKColors.Magenta,
-                StrokeWidth = 50
-            };
-
-            SKPaint fillPaint = new SKPaint
-            {
-                Style = SKPaintStyle.Fill,
-                Color = SKColors.Cyan
-            };
-
-            // Fill and stroke the path
-            canvas.DrawPath(path, fillPaint);
-            canvas.DrawPath(path, strokePaint);
-        }*/
+                canvas.DrawCircle(new SKPoint(origin.X + i * stepX + stepX / 2, origin.Y + (temps[i] - minTemp) * pixelPerDegree + 30), 10, graphPaint2);
+                path.LineTo(origin.X + i * stepX + stepX / 2, origin.Y + (temps[i] - minTemp) * pixelPerDegree + 30);
+            }
+            //path.Close();
+            canvas.DrawPath(path, graphPaint);
+        }
     }
 }
