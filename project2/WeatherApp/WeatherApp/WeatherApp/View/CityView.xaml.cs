61fdae18d8f94885e9df69ef8e23e29e731195a9
Sérgio Salgado <sergiodias.17@hotmail.com>
2019-12-12 23:47:21 +0000

    Fix graph

--- project2/WeatherApp/WeatherApp/WeatherApp/View/CityView.xaml.cs
+++ project2/WeatherApp/WeatherApp/WeatherApp/View/CityView.xaml.cs
@@ -79,7 +79,7 @@ namespace WeatherApp.View
             SKPaint coorPaint2 = new SKPaint
             {      // paint for the axis and text
                 Style = SKPaintStyle.Stroke,
-                Color = SKColors.Gray,
+                Color = SKColors.LightGray,
                 StrokeWidth = 2,
                 StrokeCap = SKStrokeCap.Round,
                 TextSize = 30
@@ -107,7 +107,7 @@ namespace WeatherApp.View
 
             //draw X and Y axis;
             canvas.DrawLine(origin, bottomRight, coorPaint);
-            canvas.DrawLine(origin, topLeft, coorPaint);
+            canvas.DrawLine(origin, new SKPoint(topLeft.X, topLeft.Y - 30), coorPaint);
 
             //draw hour text
             int steps = hours.Count();
@@ -115,20 +115,20 @@ namespace WeatherApp.View
             for(int i = 0; i < steps; i++)
             {
                 canvas.DrawLine(new SKPoint(origin.X + i * stepX, origin.Y), new SKPoint(origin.X + i * stepX, origin.Y + 15), coorPaint); //draw guide lines on X axis
-                canvas.DrawText(hours[i], origin.X + i * stepX + stepX / 3, origin.Y + 40, coorPaint); //draw '00h' text
+                canvas.DrawText(hours[i], origin.X + i * stepX + stepX / 5, origin.Y + 40, coorPaint); //draw '00h' text
             }
 
             float maxTemp = temps.Max();
             float minTemp = temps.Min();
             //calculate temperature value per pixel in Y axis
-            float pixelPerDegree = (topLeft.Y - origin.Y) / (maxTemp - minTemp);
+            float pixelPerDegree = Math.Abs((topLeft.Y - origin.Y) / (maxTemp - minTemp));
             float stepY = (origin.Y - topLeft.Y) / 4;
 
             //draw guide lines on Y axis
             for(int i = 0; i < 4; i++)
             {
                 float guidelineTemp = maxTemp - i * ((maxTemp - minTemp) / 4);
-                canvas.DrawLine(new SKPoint(origin.X, topLeft.Y + 30 + i * stepY), new SKPoint(bottomRight.X, topLeft.Y + 30 + i * stepY), coorPaint2);
+                canvas.DrawLine(new SKPoint(origin.X, topLeft.Y + i * stepY), new SKPoint(bottomRight.X, topLeft.Y + i * stepY), coorPaint2);
                 if(guidelineTemp != maxTemp && guidelineTemp != minTemp)
                 {
                     canvas.DrawText(guidelineTemp.ToString(), origin.X - 80, topLeft.Y + 30 + i * stepY, coorPaint);
@@ -136,19 +136,19 @@ namespace WeatherApp.View
             }
 
             canvas.DrawLine(origin, new SKPoint(origin.X - 15, origin.Y), coorPaint);
-            canvas.DrawLine(new SKPoint(topLeft.X, topLeft.Y + 30), new SKPoint(topLeft.X - 15, topLeft.Y + 30), coorPaint);
+            canvas.DrawLine(new SKPoint(topLeft.X, topLeft.Y), new SKPoint(topLeft.X - 15, topLeft.Y), coorPaint);
             canvas.DrawText(minTemp.ToString(), origin.X - 80, origin.Y - 15, coorPaint); //estes + e - 15 são ajustes para tornar o número mais legível
             canvas.DrawText(maxTemp.ToString(), topLeft.X - 80, topLeft.Y + 15, coorPaint);
 
             SKPath path = new SKPath();
 
-            path.MoveTo(origin.X + stepX / 2, origin.Y + (temps[0] - minTemp) * pixelPerDegree);
-            canvas.DrawCircle(new SKPoint(origin.X + stepX / 2, origin.Y + (temps[0] - minTemp) * pixelPerDegree), 10, graphPaint2);
+            path.MoveTo(origin.X + stepX / 2, origin.Y - (temps[0] - minTemp) * pixelPerDegree);
+            canvas.DrawCircle(new SKPoint(origin.X + stepX / 2, origin.Y - (temps[0] - minTemp) * pixelPerDegree), 10, graphPaint2);
 
             for(int i = 1; i < temps.Count(); i++)
             {
-                canvas.DrawCircle(new SKPoint(origin.X + i * stepX + stepX / 2, origin.Y + (temps[i] - minTemp) * pixelPerDegree + 30), 10, graphPaint2);
-                path.LineTo(origin.X + i * stepX + stepX / 2, origin.Y + (temps[i] - minTemp) * pixelPerDegree + 30);
+                canvas.DrawCircle(new SKPoint(origin.X + i * stepX + stepX / 2, origin.Y - (temps[i] - minTemp) * pixelPerDegree), 10, graphPaint2);
+                path.LineTo(origin.X + i * stepX + stepX / 2, origin.Y - (temps[i] - minTemp) * pixelPerDegree);
             }
             //path.Close();
             canvas.DrawPath(path, graphPaint);
